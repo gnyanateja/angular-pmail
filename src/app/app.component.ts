4da11e0b1c0f46472d4fbfb6022c5c2f269788b9
teja <narutoteja@gmail.com>
2019-02-27 21:03:22 +0530

    make it better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import {UserService} from './user.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pmail';
+  username = '';
+
+
+constructor (private userservice: UserService, private router: Router) {
+
+
 }
+isloggedin() {
+  if (localStorage.getItem('token') != null) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+openNav() {
+  document.getElementById('mySidenav').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+}
+
+closeNav() {
+  document.getElementById('mySidenav').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+}
+
+
+loggedout() {
+  localStorage.removeItem('token');
+  this.router.navigateByUrl('/login');
+}
+
+
+
+}
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {UserService} from './user.service';
+import { InboxComponent } from './inbox/inbox.component';
+import { MsgComponent } from './msg/msg.component';
+
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'inbox', component: InboxComponent},
+  {path: 'msg', component: MsgComponent},
+  {path: '', redirectTo: 'login', pathMatch: 'full'}, // pathmatch is used for checking exact match
+
+];
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    InboxComponent,
+    MsgComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
